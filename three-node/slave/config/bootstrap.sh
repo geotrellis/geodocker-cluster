1aded6cb1d7ac290e62f8cc988f91b15e6be129f
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-02 16:29:34 +0300

    fix ssh

--- three-node/slave/config/bootstrap.sh
+++ three-node/slave/config/bootstrap.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/svscan /etc/service/ &
 sleep 4
-sudo -u hduser bash -c "echo $ZOOKEEPER_ID > /var/zookeeper/myid"
-# tail -f $HADOOP_INSTALL/logs/*
+su hduser -c "echo $ZOOKEEPER_ID > /var/zookeeper/myid"
+#tail -f $HADOOP_INSTALL/logs/*
 while true; do sleep 1; done

c7187e7d482da728632a3f02d8f0f907c852b308
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-03 15:43:21 +0300

    fix bootstrap

--- single-node/master/config/bootstrap.sh
+++ single-node/master/config/bootstrap.sh
@@ -4,16 +4,16 @@
 /usr/bin/svscan /etc/service/ &
 sleep 4
 sudo -u hduser bash -c "echo $ZOOKEEPER_ID > /var/zookeeper/myid"
-sudo -u hduser bash -c "ssh -l hduser master1.gt 'bash -l -c \"zkServer.sh start\"'"
-# su hduser -c "ssh -l hduser slave1.gt 'bash -l -c \"zkServer.sh start\"'"
-# su hduser -c "ssh -l hduser slave2.gt 'bash -l -c \"zkServer.sh start\"'"
+sudo -u hduser bash -c ". /home/hduser/.bashrc; ssh -l hduser master1.gt 'bash -l -c \"zkServer.sh start\"'"
+# su hduser -c ". /home/hduser/.bashrc; ssh -l hduser slave1.gt 'bash -l -c \"zkServer.sh start\"'"
+# su hduser -c ". /home/hduser/.bashrc;; ssh -l hduser slave2.gt 'bash -l -c \"zkServer.sh start\"'"
 if [ "$NODE_TYPE" = "m" ]; then
-   sudo -u hduser bash -c "$HADOOP_INSTALL/sbin/start-dfs.sh"
-   sudo -u hduser bash -c "$HADOOP_INSTALL/sbin/start-yarn.sh"
+   sudo -u hduser bash -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-dfs.sh"
+   sudo -u hduser bash -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-yarn.sh"
 fi
 sleep 4
-sudo -u hduser bash -c "$ACCUMULO_HOME/bin/accumulo init --instance-name gis --password secret"
-sudo -u hduser bash -c "$ACCUMULO_HOME/bin/start-all.sh"
-sudo -u hduser bash -c "$SPARK_HOME/sbin/start-all.sh"
-# tail -f $HADOOP_INSTALL/logs/*
+sudo -u hduser bash -c ". /home/hduser/.bashrc; $ACCUMULO_HOME/bin/accumulo init --instance-name gis --password secret"
+sudo -u hduser bash -c ". /home/hduser/.bashrc; $ACCUMULO_HOME/bin/start-all.sh"
+sudo -u hduser bash -c ". /home/hduser/.bashrc; $SPARK_HOME/sbin/start-all.sh"
+# tail -f $HADOOP_INSTALL/logs/*s
 while true; do sleep 1; done
--- two-node/master/config/bootstrap.sh
+++ two-node/master/config/bootstrap.sh
@@ -4,16 +4,16 @@
 /usr/bin/svscan /etc/service/ &
 sleep 4
 su hduser -c "echo $ZOOKEEPER_ID > /var/zookeeper/myid"
-su hduser -c "ssh -l hduser master1.gt 'bash -l -c \"zkServer.sh start\"'"
-su hduser -c "ssh -l hduser slave1.gt 'bash -l -c \"zkServer.sh start\"'"
+su hduser -c ". /home/hduser/.bashrc; ssh -l hduser master1.gt 'bash -l -c \"zkServer.sh start\"'"
+su hduser -c ". /home/hduser/.bashrc; ssh -l hduser slave1.gt 'bash -l -c \"zkServer.sh start\"'"
 # su hduser -c "ssh -l hduser slave2.gt 'bash -l -c \"zkServer.sh start\"'"
 if [ "$NODE_TYPE" = "m" ]; then
-   su hduser -c "$HADOOP_INSTALL/sbin/start-dfs.sh"
-   su hduser -c "$HADOOP_INSTALL/sbin/start-yarn.sh"
+   su hduser -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-dfs.sh"
+   su hduser -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-yarn.sh"
 fi
 sleep 4
-su hduser -c "$ACCUMULO_HOME/bin/accumulo init --instance-name gis --password secret"
-su hduser -c "$ACCUMULO_HOME/bin/start-all.sh"
-su hduser -c "$SPARK_HOME/sbin/start-all.sh"
+su hduser -c ". /home/hduser/.bashrc; $ACCUMULO_HOME/bin/accumulo init --instance-name gis --password secret"
+su hduser -c ". /home/hduser/.bashrc; $ACCUMULO_HOME/bin/start-all.sh"
+su hduser -c ". /home/hduser/.bashrc; $SPARK_HOME/sbin/start-all.sh"
 # tail -f $HADOOP_INSTALL/logs/*
 while true; do sleep 1; done

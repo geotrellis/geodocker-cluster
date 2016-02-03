400bad0da40d0d9e2bae0e3956cd12058e28a1d5
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-03 18:30:32 +0300

    script to add slaves to already runing cluster

---
+++ two-node/add-slave.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in    
+    -t=*|--tag=*)
+    TAG="${i#*=}"
+    shift
+    ;;
+    -n=*|--name=*)
+    NAME="${i#*=}"
+    shift
+    ;;
+    -h=*|--host=*)
+    HOSTNAME="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+TAG=${TAG:-latest}
+
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" slave1)
+
+docker run -d -t --dns 127.0.0.1 \
+           -e NODE_TYPE=sd \
+           -e ZOOKEEPER_ID=4 \
+           -e JOIN_IP=$FIRST_IP \
+           -e HOSTNAME=$HOSTNAME --name $NAME -h $HOSTNAME daunnc/geo-slave-twn:${TAG}
--- two-node/slave/config/bootstrap.sh
+++ two-node/slave/config/bootstrap.sh
@@ -3,5 +3,18 @@
 /usr/bin/svscan /etc/service/ &
 sleep 4
 su hduser -c "echo $ZOOKEEPER_ID > /var/zookeeper/myid"
+
+# slave dynamic
+if [ "$NODE_TYPE" = "sd" ]; then
+   su hduser -c "echo $HOSTNAME > $HADOOP_INSTALL/etc/hadoop/slaves"
+   su hduser -c "echo $HOSTNAME > $ACCUMULO_HOME/conf/slaves"
+   su hduser -c "echo $HOSTNAME > $SPARK_HOME/conf/slaves"
+
+   su hduser -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-dfs.sh"
+   su hduser -c ". /home/hduser/.bashrc; $HADOOP_INSTALL/sbin/start-yarn.sh"
+   su hduser -c ". /home/hduser/.bashrc; $ACCUMULO_HOME/bin/start-here.sh"
+   su hduser -c ". /home/hduser/.bashrc; $SPARK_HOME/sbin/start-slave.sh"
+fi
+
 # tail -f $HADOOP_INSTALL/logs/*
 while true; do sleep 1; done

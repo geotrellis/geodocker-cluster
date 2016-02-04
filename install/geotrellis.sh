b1e8ec8933eaf8dfa40448a0041dc675089d8287
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-04 15:47:43 +0300

    fix install scripts

--- install/geotrellis.sh
+++ install/geotrellis.sh
@@ -1,3 +1,3 @@
 ACCUMULO_HOME=/usr/local/accumulo
 
-docker exec -it master1 git clone https://github.com/geotrellis/geotrellis.git && cd geotrellis && ./publish-local.sh                        
+docker exec -it master1 basch -c "git clone https://github.com/geotrellis/geotrellis.git && cd geotrellis && ./publish-local.sh"                        "
--- install/geowave.sh
+++ install/geowave.sh
@@ -1,12 +1,12 @@
 ACCUMULO_HOME=/usr/local/accumulo
 
-docker exec -it master1 rm -r ./geowave && git clone https://github.com/ngageoint/geowave.git && cd ./geowave && \
+docker exec -it master1 bash -c "rm -r ./geowave && git clone https://github.com/ngageoint/geowave.git && cd ./geowave && \
                         mvn install -Dmaven.test.skip=true && mvn install -Dmaven.test.skip=true && \
                         mvn package -P geowave-tools-singlejar -Dmaven.test.skip=true && \
                         mvn package -P geotools-container-singlejar -Dmaven.test.skip=true && \
                         mvn package -P accumulo-container-singlejar -Dmaven.test.skip=true && \
                         hadoop fs -mkdir -p /accumulo/general-classpath && \
                         hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.0-SNAPSHOT-accumulo-singlejar.jar /accumulo/general-classpath/ && \
-                        accumulo shell -u root -p secret -e "createuser geowave" && \
-                        accumulo shell -u root -p secret -e "createnamespace geowave" && \ 
-                        accumulo shell -u root -p secret -e "grant NameSpace.CREATE_TABLE -ns geowave -u geowave"
+                        accumulo shell -u root -p secret -e \"createuser geowave\" && \
+                        accumulo shell -u root -p secret -e \"createnamespace geowave\" && \ 
+                        accumulo shell -u root -p secret -e \"grant NameSpace.CREATE_TABLE -ns geowave -u geowave\""

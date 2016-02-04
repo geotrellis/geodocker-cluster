a4ee7de65bcaf1970bbd8a3f0072811b8f660b62
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-04 15:39:05 +0300

    fix install scripts

--- install/geowave.sh
+++ install/geowave.sh
@@ -5,8 +5,8 @@ docker exec -it master1 git clone https://github.com/ngageoint/geowave.git && cd
                         mvn package -P geowave-tools-singlejar -Dmaven.test.skip=true && \
                         mvn package -P geotools-container-singlejar -Dmaven.test.skip=true && \
                         mvn package -P accumulo-container-singlejar -Dmaven.test.skip=true && \
+                        hadoop fs -mkdir -p /accumulo/general-classpath && \
                         hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.0-SNAPSHOT-accumulo-singlejar.jar /accumulo/general-classpath/ && \
                         accumulo shell -u root -p secret -e "createuser geowave" && \
                         accumulo shell -u root -p secret -e "createnamespace geowave" && \ 
-                        accumulo shell -u root -p secret -e "grant NameSpace.CREATE_TABLE -ns geowave -u geowave" && \
-                        accumulo shell -u root -p secret -e "config -ns geowave -s table.classpath.context=geowave"
+                        accumulo shell -u root -p secret -e "grant NameSpace.CREATE_TABLE -ns geowave -u geowave"

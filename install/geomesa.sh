0637229b8bc225db242e9fb65b8a9fac6c121224
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-05 11:55:55 +0300

    fix accumulo-site settings

--- install/geomesa.sh
+++ install/geomesa.sh
@@ -3,8 +3,8 @@ ACCUMULO_HOME=/usr/local/accumulo
 docker exec -it master1 bash -c ". ~/.bashrc && rm -rf ./geomesa && \
                                  git clone https://github.com/locationtech/geomesa.git && cd ./geomesa && \
                                  mvn clean install -Dmaven.test.skip=true && \
-                                 hadoop fs -mkdir -p /accumulo/general-classpath && \
-                                 hadoop fs -copyFromLocal geomesa-accumulo/geomesa-accumulo-distributed-runtime/target/geomesa-accumulo-distributed-runtime-1.2.0-SNAPSHOT.jar /accumulo/general-classpath/ && \
-                                 hadoop fs -copyFromLocal ~/.m2/repository/joda-time/joda-time/2.3/joda-time-2.3.jar /accumulo/general-classpath/ && cd ../ && \
+                                 hadoop fs -mkdir -p /accumulo/system-classpath && \
+                                 hadoop fs -copyFromLocal geomesa-accumulo/geomesa-accumulo-distributed-runtime/target/geomesa-accumulo-distributed-runtime-1.2.0-SNAPSHOT.jar /accumulo/system-classpath/ && \
+                                 hadoop fs -copyFromLocal ~/.m2/repository/joda-time/joda-time/2.3/joda-time-2.3.jar /accumulo/system-classpath/ && cd ../ && \
                                  git clone https://github.com/geomesa/geomesa-tutorials.git && cd ./geomesa-tutorials && \
                                  mvn clean install -f geomesa-quickstart-accumulo/pom.xml -Dmaven.test.skip=true"
--- install/geowave.sh
+++ install/geowave.sh
@@ -6,8 +6,8 @@ docker exec -it master1 bash -c ". ~/.bashrc && rm -rf ./geowave && git clone ht
                                  mvn package -P geowave-tools-singlejar -Dmaven.test.skip=true && \
                                  mvn package -P geotools-container-singlejar -Dmaven.test.skip=true && \
                                  mvn package -P accumulo-container-singlejar -Dmaven.test.skip=true && \
-                                 hadoop fs -mkdir -p /accumulo/general-classpath && \
-                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.1-SNAPSHOT-accumulo-singlejar.jar /accumulo/general-classpath/ && \
+                                 hadoop fs -mkdir -p /accumulo/system-classpath && \
+                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.1-SNAPSHOT-accumulo-singlejar.jar /accumulo/system-classpath/ && \
                                  accumulo shell -u root -p secret -e \"createuser geowave\" && \
                                  accumulo shell -u root -p secret -e \"createnamespace geowave\" && \ 
                                  accumulo shell -u root -p secret -e \"grant NameSpace.CREATE_TABLE -ns geowave -u geowave\""

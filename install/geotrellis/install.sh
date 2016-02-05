839687ea5a3bd293b90573296b10a7955535a6f9
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-05 17:23:25 +0300

    install geotrellis chatta-demo

--- install/geotrellis/install.sh
+++ install/geotrellis/install.sh
@@ -2,4 +2,6 @@ ACCUMULO_HOME=/usr/local/accumulo
 
 docker exec -it master1 basch -c ". ~/.bashrc && \
                                   rm -rf geotrellis && git clone https://github.com/geotrellis/geotrellis.git && \
-                                  cd ./geotrellis && ./publish-local.sh"
+                                  cd ./geotrellis && ./publish-local.sh && cd ../ && \
+                                  git clone https://github.com/pomadchin/geotrellis-chatta-demo.git && \
+                                  cd ./geotrellis-chatta-demo && git checkout spark-version && ./sbt assembly"

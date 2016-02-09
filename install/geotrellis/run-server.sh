3f102664a05c188bc3a24e05774c7c4d4838668a
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-09 12:53:51 +0300

    fix geotrellis run scripts

--- install/geotrellis/run-server.sh
+++ install/geotrellis/run-server.sh
@@ -1,1 +1,1 @@
-docker exec -it master1 bash -c ". ~/.bashrc; java -cp ./geotrellis-chatta-demo/geotrellis/target/scala-2.10/GeoTrellis-Tutorial-Project-assembly-0.1-SNAPSHOT.jar geotrellis.chatta.Main"
+docker exec -it master1 bash -c ". ~/.bashrc; cd ./geotrellis-chatta-demo/geotrellis && java -cp target/scala-2.10/GeoTrellis-Tutorial-Project-assembly-0.1-SNAPSHOT.jar geotrellis.chatta.Main"

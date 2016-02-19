1b3ab01f4ba2e3ffc763e416d69542a4fb7d53db
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-19 22:38:55 +0300

    move geomesa and geowave to stable versions

--- install/geowave/install.sh
+++ install/geowave/install.sh
@@ -5,12 +5,13 @@ export GEOTOOLS_VERSION=14.2
 export GEOSERVER_VERSION=2.8.2
 
 docker exec -it master1 bash -c ". ~/.bashrc && rm -rf ./geowave && git clone https://github.com/ngageoint/geowave.git && cd ./geowave && \
+                                 git checkout v0.9.0 && \
                                  mvn clean install -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  mvn install -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  mvn package -P geowave-tools-singlejar -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  mvn package -P accumulo-container-singlejar -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  hadoop fs -mkdir -p /accumulo/system-classpath && \
-                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.1-SNAPSHOT-accumulo-singlejar.jar /accumulo/system-classpath/ && \
+                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.0-accumulo-singlejar.jar /accumulo/system-classpath/ && \
                                  accumulo shell -u root -p secret -e \"createuser geowave\" && \
                                  accumulo shell -u root -p secret -e \"createnamespace geowave\" && \
                                  accumulo shell -u root -p secret -e \"grant NameSpace.CREATE_TABLE -ns geowave -u geowave\""
--- install/geowave/update.sh
+++ install/geowave/update.sh
@@ -11,5 +11,5 @@ docker exec -it master1 bash -c ". ~/.bashrc && \
                                  mvn package -P geowave-tools-singlejar -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  mvn package -P accumulo-container-singlejar -Dmaven.test.skip=true -Dhadoop.version=${HADOOP_VERSION} -Dgeotools.version=${GEOTOOLS_VERSION} -Dgeoserver.version=${GEOSERVER_VERSION} && \
                                  hadoop fs -mkdir -p /accumulo/system-classpath && \
-                                 hadoop fs -rm /accumulo/system-classpath/geowave-deploy-0.9.1-SNAPSHOT-accumulo-singlejar.jar && \
-                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.1-SNAPSHOT-accumulo-singlejar.jar /accumulo/system-classpath/"
+                                 hadoop fs -rm /accumulo/system-classpath/geowave-deploy-0.9.0-accumulo-singlejar.jar && \
+                                 hadoop fs -copyFromLocal deploy/target/geowave-deploy-0.9.0-accumulo-singlejar.jar /accumulo/system-classpath/"

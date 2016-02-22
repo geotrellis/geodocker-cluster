096724e25eca40ca7ff61bcf58c4fb5d5cefce06
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-23 01:25:17 +0300

    build scripts fix

--- base/build.sh
+++ base/build.sh
@@ -3,6 +3,10 @@
 for i in "$@"
 do
 case $i in
+    -s=*|--scala=*)
+    SCALA_VERSION="${i#*=}"
+    shift
+    ;;
     -a=*|--accumulo=*)
     ACCUMULO_VERSION="${i#*=}"
     shift 
@@ -24,7 +28,7 @@ ACCUMULO_VERSION=${ACCUMULO_VERSION:-1.7.0}
 TAG=${TAG:-latest}
 PUBLISH=${PUBLISH:-false}
 
-docker build -t daunnc/geodocker-base:${TAG} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} . 
+docker build -t daunnc/geodocker-base:${TAG} --build-arg SCALA_VERSION=${SCALA_VERSION} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} . 
 
 if ${PUBLISH}; then
   docker push daunnc/geodocker-base:${TAG}
--- nodes/build.sh
+++ nodes/build.sh
@@ -3,14 +3,6 @@
 for i in "$@"
 do
 case $i in
-    -s=*|--scala=*)
-    SCALA_VERSION="${i#*=}"
-    shift
-    ;;
-    -a=*|--accumulo=*)
-    ACCUMULO_VERSION="${i#*=}"
-    shift
-    ;;
     -t=*|--tag=*)
     TAG="${i#*=}"
     shift
@@ -32,7 +24,6 @@ case $i in
 esac
 done
 
-SCALA_VERSION=${SCALA_VERSION:-2.10}
 ACCUMULO_VERSION=${ACCUMULO_VERSION:-1.7.0}
 TAG=${TAG:-latest}
 BASE_TAG=${BASE_TAG:-latest}
@@ -53,8 +44,8 @@ if ${BUILD_BASE}; then
   cd ~-
 fi
 
-docker build -t daunnc/geodocker-master:${TAG} --build-arg SCALA_VERSION=${SCALA_VERSION} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} -f DockerfileMaster .
-docker build -t daunnc/geodocker-slave:${TAG} --build-arg SCALA_VERSION=${SCALA_VERSION} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} -f DockerfileSlave .
+docker build -t daunnc/geodocker-master:${TAG} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} -f DockerfileMaster .
+docker build -t daunnc/geodocker-slave:${TAG} --build-arg ACCUMULO_VERSION=${ACCUMULO_VERSION} -f DockerfileSlave .
 
 if ${PUBLISH}; then
   docker push daunnc/geodocker-master:${TAG}

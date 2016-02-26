083a2537aef69aa1d6b7737c4255bc7b6cca23f8
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-02-26 11:38:40 +0300

    fix geotrellis scripts path

--- install/geotrellis/install.sh
+++ install/geotrellis/install.sh
@@ -15,10 +15,10 @@ done
 SCALA_VERSION=${SCALA_VERSION:-"2.10.5"}
 
 docker exec -it master1 bash -c ". ~/.bashrc && \
-                                  rm -rf geotrellis && git clone https://github.com/geotrellis/geotrellis.git && \
-                                  cd ./geotrellis && ./publish-local.sh && cd ../ && \
-                                  rm -rf geotrellis-chatta-demo && git clone https://github.com/pomadchin/geotrellis-chatta-demo.git && \
-                                  cd ./geotrellis-chatta-demo/geotrellis && git checkout spark-version && ./sbt -Dscala.version=${SCALA_VERSION} assembly"
+                                  rm -rf geotrellis; git clone https://github.com/geotrellis/geotrellis.git && \
+                                  cd ./geotrellis; ./scripts/publish-local.sh; cd ../ && \
+                                  rm -rf geotrellis-chatta-demo; git clone https://github.com/pomadchin/geotrellis-chatta-demo.git && \
+                                  cd ./geotrellis-chatta-demo/geotrellis; git checkout spark-version; ./sbt -Dscala.version=${SCALA_VERSION} assembly"
 
 docker cp fs/resources master1:/data/geotrellis-chatta-demo/geotrellis/src/main/
 docker cp fs/ingest.sh master1:/data/geotrellis-chatta-demo/geotrellis/

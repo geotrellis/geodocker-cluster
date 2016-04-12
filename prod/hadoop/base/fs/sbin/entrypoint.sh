013cb4f8e8264fde9567b357c2fd8f9a70638086
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-04-12 12:55:36 +0300

    fix entry points

--- prod/hadoop/base/fs/sbin/entrypoint.sh
+++ prod/hadoop/base/fs/sbin/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -eo pipefail
 
-if [[ -v ${HADOOP_MASTER_ADDRESS} ]]; then
+if [[ ! -v ${HADOOP_MASTER_ADDRESS} ]]; then
   sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${HADOOP_CONF_DIR}/core-site.xml
 fi
 
--- prod/hadoop/sname/fs/sbin/entrypoint.sh
+++ prod/hadoop/sname/fs/sbin/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ -v ${HADOOP_MASTER_ADDRESS} ]]; then
+if [[ ! -v ${HADOOP_MASTER_ADDRESS} ]]; then
   sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${HADOOP_CONF_DIR}/core-site.xml
 fi
 

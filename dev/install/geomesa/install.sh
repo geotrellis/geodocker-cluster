bae974e01ddbd631926e48215271f2fe59482516
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-04-13 22:58:03 +0300

    fix geomesa install script

--- dev/install/geomesa/install.sh
+++ dev/install/geomesa/install.sh
@@ -7,6 +7,6 @@ docker exec -it master1 bash -c ". ~/.bashrc; rm -rf ./geomesa && \
                                  hadoop fs -mkdir -p /accumulo/system-classpath && \
                                  hadoop fs -copyFromLocal geomesa-accumulo/geomesa-accumulo-distributed-runtime/target/geomesa-accumulo-distributed-runtime-1.2.1.jar /accumulo/system-classpath/ && \
                                  hadoop fs -copyFromLocal ~/.m2/repository/joda-time/joda-time/2.3/joda-time-2.3.jar /accumulo/system-classpath/ && cd ../ && \
-                                 git clone https://github.com/geomesa/geomesa-tutorials.git; cd ./geomesa-tutorials && \
+                                 git clone https://github.com/geomesa/geomesa-tutorials.git && cd ./geomesa-tutorials && \
                                  git checkout geomesa-tutorials-1.2.1 && \
                                  mvn clean install -f geomesa-quickstart-accumulo/pom.xml -Dmaven.test.skip=true"

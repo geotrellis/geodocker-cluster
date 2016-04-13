abd96b7c1d4cd2daefff5a1b9c1df20f822417d0
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-04-13 23:00:16 +0300

    changed chatta repo to the official repo

--- dev/install/geotrellis/install.sh
+++ dev/install/geotrellis/install.sh
@@ -17,8 +17,8 @@ SCALA_VERSION=${SCALA_VERSION:-"2.10.5"}
 docker exec -it master1 bash -c ". ~/.bashrc && \
                                   rm -rf geotrellis; git clone https://github.com/geotrellis/geotrellis.git && \
                                   cd ./geotrellis; git checkout v0.10.0-RC4 && ./scripts/publish-local.sh; cd ../ && \
-                                  rm -rf geotrellis-chatta-demo; git clone https://github.com/pomadchin/geotrellis-chatta-demo.git && \
-                                  cd ./geotrellis-chatta-demo/geotrellis; git checkout spark-version"
+                                  rm -rf geotrellis-chatta-demo; git clone https://github.com/geotrellis/geotrellis-chatta-demo.git && \
+                                  cd ./geotrellis-chatta-demo/geotrellis"
 
 docker cp fs/resources master1:/data/geotrellis-chatta-demo/geotrellis/src/main/
 docker cp fs/ingest.sh master1:/data/geotrellis-chatta-demo/geotrellis/

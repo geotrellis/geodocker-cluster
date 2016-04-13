91ee5aca4f265ebd9097eeac9edd7765c6776954
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-04-13 21:26:16 +0300

    fix gt install script

--- dev/install/geotrellis/install.sh
+++ dev/install/geotrellis/install.sh
@@ -18,7 +18,9 @@ docker exec -it master1 bash -c ". ~/.bashrc && \
                                   rm -rf geotrellis; git clone https://github.com/geotrellis/geotrellis.git && \
                                   cd ./geotrellis; git checkout v0.10.0-RC4 && ./scripts/publish-local.sh; cd ../ && \
                                   rm -rf geotrellis-chatta-demo; git clone https://github.com/pomadchin/geotrellis-chatta-demo.git && \
-                                  cd ./geotrellis-chatta-demo/geotrellis; git checkout spark-version; ./sbt -Dscala.version=${SCALA_VERSION} assembly"
+                                  cd ./geotrellis-chatta-demo/geotrellis; git checkout spark-version"
 
 docker cp fs/resources master1:/data/geotrellis-chatta-demo/geotrellis/src/main/
 docker cp fs/ingest.sh master1:/data/geotrellis-chatta-demo/geotrellis/
+
+docker exec -it master1 bash -c ". ~/.bashrc; cd ./geotrellis-chatta-demo/geotrellis; ./sbt -Dscala.version=${SCALA_VERSION} assembly"

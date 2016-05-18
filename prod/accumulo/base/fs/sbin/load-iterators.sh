a8eceaf274e6eb2048d322d07c54b208fabc3258
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-05-18 13:58:52 +0300

    fix load-iterators.sh

--- prod/accumulo/base/fs/sbin/load-iterators.sh
+++ prod/accumulo/base/fs/sbin/load-iterators.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-if [ ${WITH_GEO_ITERATORS} ]; then
+if ${WITH_GEO_ITERATORS}; then
   cd ${ACCUMULO_HOME}/lib/ext/ 
   wget https://bitbucket.org/pomadchin/binary/raw/c5b0ea0aa98de751f56848d9915a9b6a4ee8f3df/geo/geomesa-accumulo-distributed-runtime-1.2.1.jar
   wget https://bitbucket.org/pomadchin/binary/raw/c5b0ea0aa98de751f56848d9915a9b6a4ee8f3df/geo/joda-time-2.3.jar

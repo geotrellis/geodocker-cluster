28d7410a798ecaed5a2c43b0ecc8bd28ca61ae13
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-05-18 15:51:12 +0300

    fix entrypoints

--- prod/accumulo/master/fs/sbin/entrypoint-dev.sh
+++ prod/accumulo/master/fs/sbin/entrypoint-dev.sh
@@ -23,7 +23,7 @@ fi
 
 echo -n "Waiting for TCP connection to ${HADOOP_MASTER_ADDRESS}:${PORT}..."
 
-while ! nc -w 1 HADOOP_MASTER_ADDRESS PORT 2>/dev/null
+while ! nc -w 1 ${HADOOP_MASTER_ADDRESS} ${PORT} 2>/dev/null
 do
   echo -n .
   sleep 1

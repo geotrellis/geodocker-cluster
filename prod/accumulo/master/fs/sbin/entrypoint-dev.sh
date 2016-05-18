3ee176f521bb55e761341bd85dd7f32e69721475
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-05-18 15:44:07 +0300

    fix accumulo master entry point

--- prod/accumulo/master/fs/sbin/entrypoint-dev.sh
+++ prod/accumulo/master/fs/sbin/entrypoint-dev.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-export PORT="50010"
+export PORT="50070"
 
 if [[ ! -v ${HADOOP_MASTER_ADDRESS} ]]; then
   sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${HADOOP_CONF_DIR}/core-site.xml
